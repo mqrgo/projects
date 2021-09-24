71777f27f70f8e49a3172ca6980bf7725253af57
mqrgo <90678432+mqrgo@users.noreply.github.com>
2021-09-24 13:31:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('test');
+const mysql = require("mysql2");
+  
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "usersdb",
+  password: "пароль_от_сервера"
+});
